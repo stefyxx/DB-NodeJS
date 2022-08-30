ada2f645378d7fdf40f005db13bf3db40e42257c
stefyxx <86487968+stefyxx@users.noreply.github.com>
2022-08-30 09:56:51 +0200

    colonne-> case sensibili

--- index.js
+++ index.js
@@ -6,9 +6,12 @@ async function myConnection(query) {
     const client = new pg.Client({
         user: 'gis',
         host: 'staging-readonly1-do-user-211558-0.b.db.ondigitalocean.com',
-        database: 'defaultdb',
+        database: 'gis',
         password: 'gis',
-        port: 25060
+        port: 25060,
+        ssl: {
+            rejectUnauthorized: false,
+        }
         //connectionString,
     })
     await client.connect();
@@ -16,9 +19,17 @@ async function myConnection(query) {
     await client.end();
     return result;
 }
+/*
+les colonnes sont CASE SENSITIVE en SQL :
+' pour les values
+” pour les colonnes
 
+Et alors: 
+SELECT * FROM "gtfsRoutes" WHERE "agencyId" = 'delijn' LIMIT 10
+
+*/
 function ProcessData() {
-    myConnection('SELECT $1::text as message', ['Hello world!'])
+    myConnection("SELECT * FROM \"gtfsRoutes\"WHERE \"agencyId\" = 'delijn' LIMIT 10")
         .then(function success(result) {
             console.log(result)
         })
